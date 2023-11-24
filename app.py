2673c24032b63ec0510c0bd22776e2d850ad1159
Felix Frank Felix <frankozie4life@gmail.com>
2023-11-24 19:35:26 +0100

    made changes

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ class FarmData(BaseModel):
 @app.get("/")
 def main():
    
-    return "EVIAN IS ACTIVE"
+    return "ACTIVE"
 
 @app.post("/api/recommedations/temperature")
 def rec_gen(data: FarmData):
